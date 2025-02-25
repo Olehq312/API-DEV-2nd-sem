d3e9ce0fe0a6a7ebfa936a97488c7246ed77e9f3
Oleh <118124646+Olehq312@users.noreply.github.com>
2025-02-25 09:30:36 +0100

    last update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
+import cors from 'cors';
 import { testConnection } from './repository/database';
 import routes from './routes';
+
+
 
 
 dotenvFlow.config();
@@ -14,6 +17,10 @@ const app: Application = express();
 export function startServer() {
 
     app.use(express.json());
+
+// add cors
+    setupCors();
+
 
     app.use('/api', routes);
 
@@ -28,4 +35,30 @@ export function startServer() {
         console.log('Server is up and running on port: ' + PORT);
     });
 }
+
+export function setupCors() {
+    console.log('Setting up CORS');
+    // kw 2-dec-2024 - Working CORS setup without credentials. Could refactor
+    app.use(
+        cors({
+            origin: "*", // Allow requests from any origin
+            // kw 29-nov-2024 - allow methods + headers + credentials
+            methods: 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE',
+            allowedHeaders: ['auth-token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'], // Allow specific headers
+            credentials: true,
+        })
+    );
+
+    // kw 2-dec-2024 - set the Access-Control-Allow-Origin header for preflight requests - console error 
+    app.options('*', (req: Request, res: Response) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'auth-token, Origin, X-Requested-With, Content-Type, Accept');
+        // test for credentials
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.sendStatus(200);
+    });
+}
+
+
 
