0870ac45a8e1fbd03bbbfc31992943c97ed6e791
Oleh <118124646+Olehq312@users.noreply.github.com>
2025-02-27 23:37:21 +0100

    cors and swagger added

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,57 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
-import cors from 'cors';
 import { testConnection } from './repository/database';
 import routes from './routes';
-
-
-
+import { setupDocs } from './util/documentations';
+import cors from 'cors';
 
 dotenvFlow.config();
 
-
+// create express application
 const app: Application = express();
 
+/**
+ * Setup CORS handling
+ */
+function setupCors() {
 
+    app.use(cors({
 
+        // Allow request from any origin
+        origin: "*",
+
+        // allow HTTP methods
+        methods: 'GET, PUT, POST, DELETE',
+
+        // allow headers
+        allowedHeaders: ['auth-token', 'Origin', 'X-Requested-Width', 'Content-Type', 'Accept'],
+
+        // allow credentials
+        credentials:true
+    }))
+}
+
+/**
+ * 
+ */
 export function startServer() {
 
+    setupCors();
+    
+    // JSON body parser
     app.use(express.json());
 
-// add cors
-    setupCors();
-
-
+    // bind routes to the app
     app.use('/api', routes);
 
+    setupDocs(app);
 
+    // test the connection to the database
     testConnection();
 
-
-    
-
+    // start the server
     const PORT: number = parseInt(process.env.PORT as string) || 4000;
     app.listen(PORT, function () {
-        console.log('Server is up and running on port: ' + PORT);
+        console.log("Server is up and running on port: " + PORT);
     });
 }
-
-export function setupCors() {
-    console.log('Setting up CORS');
-    // kw 2-dec-2024 - Working CORS setup without credentials. Could refactor
-    app.use(
-        cors({
-            origin: "*", // Allow requests from any origin
-            // kw 29-nov-2024 - allow methods + headers + credentials
-            methods: 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE',
-            allowedHeaders: ['auth-token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'], // Allow specific headers
-            credentials: true,
-        })
-    );
-
-    // kw 2-dec-2024 - set the Access-Control-Allow-Origin header for preflight requests - console error 
-    app.options('*', (req: Request, res: Response) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'auth-token, Origin, X-Requested-With, Content-Type, Accept');
-        // test for credentials
-        res.header('Access-Control-Allow-Credentials', 'true');
-        res.sendStatus(200);
-    });
-}
-
-
-
